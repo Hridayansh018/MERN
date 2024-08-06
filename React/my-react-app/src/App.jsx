62dce0946e0fb684df91a3bd89edff02dcd1ddfd
Hridayansh018 <hridayansh018@gmail.com>
2024-08-06 23:44:57 +0530

    Getting Started with react.js

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./header.jsx"
+import Footer from "./footer.jsx"
+import Card from "./card.jsx"
+
+
+function App() {
+
+  return(
+    <>
+      <Header />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ React/my-react-app/src/card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+    return(
+        <div className="card">
+            <h2>My card</h2>
+            <img src="https://picsum.photos/200" alt="#" />
+            <h3>Hridayansh Awasthi</h3>
+            <h3>17</h3>
+            <h3>Software Engineer</h3>
+        </div>
+    )
+}
+
+export default Card
